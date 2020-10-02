2038abb3a5f3e17b7d2fc364f9ca35d6addb8956
Papa-Sean <smsugrue@gmail.com>
2020-10-01 20:43:31 -0400

    added event listener to forms

--- script.js
+++ script.js
@@ -9,9 +9,19 @@ const Nmbr = document.getElementById
 ('Nmbr')
 const Special = document.getElementById
 ('Special')
+const form = document.getElementById('pwgen')
 
 AmmountRange.addEventListener('input', syncAmmountCH)
 AmmountNumber.addEventListener('input', syncAmmountCH)
+//added event listener to my forms
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = AmmountNumber.value
+  const includeUppercase = UPPERCASEE.checked
+  const includeNumbers = Nmbr.checked
+  const includeSymbols = Special.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+})
 
 //linked the slider/input boxes
 function syncAmmountCH(e) {
