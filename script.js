93f1f7318495539392eb12850caf885115ecd772
Papa-Sean <smsugrue@gmail.com>
2020-10-01 23:31:14 -0400

    notes of where I am falling short

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ const Nmbr = document.getElementById
 const Special = document.getElementById
 ('Special')
 const form = document.getElementById
-('pwgen')
-const LOWERCASEChar = lowHigh(97,122)
-const UPPERCASEChar = lowHigh(65,90)
+('passwordGeneratorForm')
+
+const LOWERCASEChar = lowHigh(65,90)
+const UPPERCASEChar = lowHigh(97,122)
 const NmbrChar = lowHigh(48,57)
 const SpecialChar = lowHigh(33, 47).concat(lowHigh(58, 64)).concat(lowHigh(91, 96)).concat(lowHigh(123, 126))
 
@@ -20,19 +21,31 @@ AmountNumber.addEventListener('input', syncAmountCH)
 //added event listener to my forms
 form.addEventListener('submit', e => {
   e.preventDefault()
-  const characterAmount = AmountNumber.value
-  const includeUppercase = UPPERCASEE.checked
-  const includeNumbers = Nmbr.checked
-  const includeSymbols = Special.checked
-  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  const AmountNumber = AmountNumberElement.value
+  const UPPERCASE = UPPERCASEEElement.checked
+  const Nmbr = NmbrElement.checked
+  const Special = SpecialElement.checked
+  const password = generatePassword(AmountNumber, UPPERCASE, Nmbr, Special)
 })
 //add function to generate password
+//whenever I add this section, the javascript links I have in place stop working
 function generatePassword(characterAmount, includeSymbols, includeUppercase, includeNumbers){
+  let charCodes = LOWERCASEChar
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASEChar)
+  if (includeNumbers) charCodes = charCodes.concat(NmbrChar)
+  if (includeSymbols) charCodes = charCodes.concat(SpecialChar)
+  const passwordCharecters []
+  for (let i = 0; i < characterAmount, i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * characterAmount)]
+    passwordCharecters.push(String.fromCharCode(charecterCode))
+    passwordCharecters.push(charecter)
+  }
 }
+return passwordCharecters.join('')
 
 function lowHigh(low,high){
   const array = []
-  for (let i= low; i <=high; i++){
+  for (let i = low; i <=high; i++){
     array.push(i)
   }
   return array
